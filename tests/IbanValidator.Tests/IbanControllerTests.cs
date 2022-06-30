aa2199cc3eaf38a78356e49c2f406a8e2f35de65
eugene_emelianov <evgeny_emelyanov@hotmail.com>
2022-06-30 16:10:58 +0300

    fixed folders structure

---
+++ tests/IbanValidator.Tests/IbanControllerTests.cs
@@ -0,0 +1,47 @@
+using Xunit;
+using Moq;
+
+namespace IbanValidator.Tests
+{
+    public class IbanControllerTests
+    {
+        private Mock<Interfaces.IIbanValidator> _ibanValidator;
+        private Controllers.IbanController _ibanController;
+
+        public IbanControllerTests()
+        {
+            _ibanValidator = new Mock<Interfaces.IIbanValidator>();
+            _ibanController = new Controllers.IbanController(_ibanValidator.Object, null);
+        }
+
+        [Fact]
+        public async Task ValidateIban_WhenIbanIsEmpty_ThenThrows()
+        {
+            // Arrange
+            var iban = "";
+            _ibanValidator.Setup(x => x.Validate(iban)).Throws<ArgumentException>();
+
+            // Act
+            var sut = await _ibanController.Validate(iban);
+
+            // Assert
+    
+            Assert.False(sut.Value);
+        }
+
+        [Fact]
+        public async Task ValidateIban_When_ThenThrows()
+        {
+            // Arrange
+            var iban = "GB82WEST12345698765432";
+            _ibanValidator.Setup(x => x.Validate(iban)).Returns(Task.FromResult(true));
+
+            // Act
+            var sut = await _ibanController.Validate(iban);
+
+            // Assert
+            Assert.True(sut.Value);
+        }
+
+    }
+}
---
+++ tests/IbanValidator.Tests/IbanValidatorTests.cs
@@ -0,0 +1,110 @@
+using Xunit;
+
+namespace IbanValidator.Tests
+{
+    public class IbanValidatorTests
+    {
+        private Services.IbanValidator _ibanValidator;
+
+        public IbanValidatorTests()
+        {
+            _ibanValidator = new Services.IbanValidator();
+        }
+
+        [Fact]
+        public async Task ValidateIban_WhenIbanIsEmpty_ThenThrows()
+        {
+            // Arrange
+            var iban = "";
+
+            // Act
+            Task sut() => _ibanValidator.Validate(iban);
+
+            // Assert
+            var ex = await Assert.ThrowsAsync<ArgumentNullException>(sut);
+            Assert.Contains($"IBAN can not be empty", ex.Message);
+        }
+
+        [Fact]
+        public async Task ValidateIban_WhenIbanHasShortNumberOfDigits_ThenThrows()
+        {
+            // Assert
+            var iban = "BE1800165492356";
+
+            // Act
+            Task sut() => _ibanValidator.Validate(iban);
+
+            // Assert
+            var ex = await Assert.ThrowsAsync<ArgumentException>(sut);
+            Assert.Contains($"The expected number of digits for country code", ex.Message);
+        }
+
+        [Fact]
+        public async Task ValidateIban_WhenIbanExceedsNumberOfDigits_ThenThrows()
+        {
+            // Assert
+            var iban = "BE180016549235656";
+
+            // Act
+            Task sut() => _ibanValidator.Validate(iban);
+
+            // Assert
+            var ex = await Assert.ThrowsAsync<ArgumentException>(sut);
+            Assert.Contains($"The expected number of digits for country code", ex.Message);
+        }
+
+        [Fact]
+        public async Task ValidateIban_WhenModulo97ValidationFails_ThenThrows()
+        {
+            // Assert
+            var iban = "BE18001654923566";
+
+            // Act
+            Task sut() => _ibanValidator.Validate(iban);
+
+            // Assert
+            var ex = await Assert.ThrowsAsync<ArgumentException>(sut);
+            Assert.Equal($"Modulo 97 validation filed for iban {iban}", ex.Message);
+        }
+
+        [Fact]
+        public async Task ValidateIban_WhenUnknowCountryCode_ThenThrows()
+        {
+            // Arrange
+            var iban = "XX82WEST12345698765432";
+            
+            // Act
+            Task sut () => _ibanValidator.Validate(iban);
+
+            // Assert
+            var ex = await Assert.ThrowsAsync<ArgumentException>(sut);
+            Assert.Equal($"Got unknow country code XX", ex.Message);
+        }
+
+        [Fact]
+        public async Task ValidateIban_WhenValidIbanNumber_ThenIsValidTrue()
+        {
+            // Arrange
+            var iban = "BE18001654923565";
+
+            // Act
+            var result = await _ibanValidator.Validate(iban);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public async Task ValidateIban_WhenValidForeignIbanNumber_ThenIsValidTrue()
+        {
+            // Arrange
+            var iban = "GB82WEST12345698765432";
+      
+            // Act
+            var result = await _ibanValidator.Validate(iban);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
