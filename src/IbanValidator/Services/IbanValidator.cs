d5ccef3e9637786d9d772e513ad80d97f6268a8f
eugene_emelianov <evgeny_emelyanov@hotmail.com>
2022-07-01 11:52:53 +0300

    cleanup

--- src/IbanValidator/Services/IbanValidator.cs
+++ src/IbanValidator/Services/IbanValidator.cs
@@ -82,12 +82,12 @@ namespace IbanValidator.Services
         /// <returns>True if the IBAN is valid and False otherwise</returns>
         public Task<bool> Validate(string iban)
         {
-            // Replace spaces
-            iban = iban.Replace(" ", "");
-
             // Validate if IBAN is null, empty or whitespace
             if (string.IsNullOrWhiteSpace(iban))
                 throw new ArgumentNullException("IBAN can not be empty");
+
+            // Replace spaces
+            iban = iban.Replace(" ", "");
 
             // Validate if country code is available
             if (iban.Length < 2)
